18d4f1d83ae413191dedefe1b9d2d5dcb4b8d743
Crew Spence <crewspence@gCrewSpence.wowway.com>
2020-11-19 06:35:10 -0500

    Wrote working solution.

--- codewars_coin_combination.js
+++ codewars_coin_combination.js
@@ -11,6 +11,12 @@ So for example:
 coinCombo(6) --> [1, 1, 0, 0]
 */
 
-var coinCombo = function(cents) {
-
+const coinCombo = (cents) => {
+    let coins = [1,5,10,25];
+    let combo = [0,0,0,0];
+    for (let i = 3; i >= 0; i--) {
+        combo[i] = Math.floor(cents / coins[i]);
+        cents -= combo[i] * coins[i];
+    }
+    return combo;
 }
